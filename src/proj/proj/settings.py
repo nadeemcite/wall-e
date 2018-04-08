c1b4599ca9e5af63290e24568316db1895e01aa1
nadeemcite <nady5730642@gmail.com>
2018-04-08 20:03:57 +0530

    settings setup

--- src/proj/proj/settings.py
+++ src/proj/proj/settings.py
@@ -118,3 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# DO NOT WRITE ANY THING BELOW THIS
+
+try:
+    from proj.local_settings import *
+except ImportError:
+    password
--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 const PORT = 8000;
 require('./rest/config')(app);
-app.listen(PORT,(err)=>{
+app.listen(PORT, (err) => {
     console.log(err || 'server running at ' + PORT);
 });
