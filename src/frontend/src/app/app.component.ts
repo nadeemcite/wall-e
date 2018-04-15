90382c1f8ba1bfdbaf91443ca8bf7cc2754a57b4
nadeemcite <nady5730642@gmail.com>
2018-04-15 10:24:27 +0530

    angular  service setup

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GeneralService } from './services/general.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(private generalService: GeneralService){
+
+  }
 }
--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {GeneralService} from './services/general.service';
 
 
 @NgModule({
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [GeneralService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
