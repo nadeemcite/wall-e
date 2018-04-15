294c3403a66e14dcd8cdc6bd0f6a6def9fe25532
nadeemcite <nady5730642@gmail.com>
2018-04-15 10:34:07 +0530

    landing component

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {LandingComponent} from './views/landing/landing.component';
 import {GeneralService} from './services/general.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, LandingComponent
   ],
   imports: [
     BrowserModule
