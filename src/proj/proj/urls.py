9f2a3504cd170a876abfb6996548058cc571e96a
vaishnaviajmera22 <vaishnaviajmera22@gmail.com>
2018-06-25 10:19:14 +0530

    the created API

--- src/proj/proj/urls.py
+++ src/proj/proj/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework.urlpatterns import format_suffix_patterns
+from webapp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^Product/', views.productList.as_view()),
 ]
